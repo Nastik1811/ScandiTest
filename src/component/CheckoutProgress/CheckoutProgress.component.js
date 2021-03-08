c3e0df34e540c2915760ed9ad0e2a4a85a58921a
Nastik1811 <lugovskaya.1999@mail.ru>
2021-03-08 19:47:18 +0300

    Update CheckoutProgress

--- src/component/CheckoutProgress/CheckoutProgress.component.js
+++ src/component/CheckoutProgress/CheckoutProgress.component.js
@@ -20,29 +20,82 @@ export class CheckoutProgressComponent extends PureComponent {
         ]).isRequired
     };
 
+    stepMap = {
+        [SHIPPING_STEP]: {
+            order: 1,
+            title: __('Shipping')
+        },
+        [BILLING_STEP]: {
+            order: 2,
+            title: __('Review & Payment')
+        },
+        [DETAILS_STEP]: {
+            order: 3
+        }
+    };
+
+    renderIcon({ step, isPassed }) {
+        if (isPassed) {
+            return (
+                <div
+                  block="CheckoutProgress"
+                  elem="CheckIcon"
+                />
+            );
+        }
+
+        return (
+            <span
+              block="CheckoutProgress"
+              elem="OrderIcon"
+            >
+                { this.stepMap[step].order }
+            </span>
+        );
+    }
+
+    renderStepTitle({ step }) {
+        return (
+            <span
+              block="CheckoutProgress"
+              elem="StepTitle"
+            >
+                { this.stepMap[step].title }
+            </span>
+        );
+    }
+
+    renderStep({ step, isPassed, isRiched }) {
+        return (
+            <div
+              block="CheckoutProgress"
+              elem="Step"
+              mods={ { isRiched } }
+            >
+                { this.renderIcon({ step, isPassed }) }
+                { this.renderStepTitle({ step }) }
+            </div>
+        );
+    }
+
+    renderLine({ isRiched }) {
+        return (
+            <div block="CheckoutProgress" elem="Line" mods={ { isRiched } } />
+        );
+    }
+
     render() {
         const { checkoutStep } = this.props;
+        const isFirstPassed = checkoutStep === BILLING_STEP || checkoutStep === DETAILS_STEP;
+        const isSecondPassed = checkoutStep === DETAILS_STEP;
+
         return (
             <div block="CheckoutProgress">
-                <div block="CheckoutProgress" elem="Line" mods={ { isPassed: true } } />
-                <div block="CheckoutProgress" elem="Step" mods={ { isPassed: true } }>1</div>
-                <div
-                  block="CheckoutProgress"
-                  elem="Line"
-                  mods={ { isPassed: checkoutStep === BILLING_STEP || checkoutStep === DETAILS_STEP } }
-                />
-                <div
-                  block="CheckoutProgress"
-                  elem="Step"
-                  mods={ { isPassed: checkoutStep === BILLING_STEP || checkoutStep === DETAILS_STEP } }
-                >
-                    2
-                </div>
-                <div
-                  block="CheckoutProgress"
-                  elem="Line"
-                  mods={ { isPassed: checkoutStep === DETAILS_STEP } }
-                />
+                { this.renderLine({ isRiched: true }) }
+                { this.renderStep({ step: SHIPPING_STEP, isPassed: isFirstPassed, isRiched: true }) }
+                { this.renderLine({ isRiched: isFirstPassed }) }
+                { this.renderStep({ step: BILLING_STEP, isPassed: isSecondPassed, isRiched: isFirstPassed }) }
+                { this.renderLine({ isRiched: isSecondPassed }) }
             </div>
         );
     }
